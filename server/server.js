025938ac50455fdc7469c8b98e295631f7dd36cd
Abdela <abdellaharegaw616@gmail.com>
2026-04-22 06:57:03 -0700

    Fix CORS for mobile frontend access

--- server/server.js
+++ server/server.js
@@ -10,8 +10,15 @@ const db = require('./mockDatabase');
 
 const app = express();
 
+// CORS - Allow all origins for now (you can restrict later)
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
